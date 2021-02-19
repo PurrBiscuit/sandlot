159d435ace32ea434ce11c0f50c7d6b6e7cc08f8
Purrbiscuit <spurr@articulate.com>
2021-02-18 19:07:37 -0500

    use ES6 syntax

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+// demonstrate how the send an error object using the next in middleware
+
+// const causeError = (req, res, next) => {
+//   console.log('whattt')
+//   next({
+//     status: 401,
+//     message: 'Something Bad Happened'
+//   })
+// }
+
+// router.use(causeError)
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', (req, res) => {
+  res.send('respond with a resource')
+})
 
-module.exports = router;
+module.exports = router
